656f443e8aa85e680f5bca42e6e92e2bdd432e11
Lion <lflorsandoval@gmail.com>
2024-05-15 23:30:08 -0500

    initials

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,22 @@ import "./index.css";
 import App from "@/App";
 import { AuthProvider } from "./context";
 import { store } from "@/hooks/store";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Provider store={store}>
-        <NextUIProvider>
-          <main className="light text-foreground bg-background">
-            <App />
-          </main>
-        </NextUIProvider>
-      </Provider>
-    </AuthProvider>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <AuthProvider>
+          <Provider store={store}>
+            <NextUIProvider>
+              <main className="light text-foreground bg-background">
+                <App />
+              </main>
+            </NextUIProvider>
+          </Provider>
+        </AuthProvider>
+      )}
+    </Authenticator>
   </React.StrictMode>
 );
